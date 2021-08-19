25b9d9d38440b107707588408b55b108a0d3a5b6
felipe <felipesantiagolds@gmail.com>
2021-08-19 16:57:01 -0300

    Socket server

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+const moment = require('moment');
+
 require('dotenv').config();
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const { PORT } = process.env;
-
-io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id} `);
-});
-
+const { PORT } = process.env || 3000;
 // app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, '/index.html'));
+
+io.on('connection', (socket) => {
+  console.log(`New connection: ${socket.id}`);
+
+  socket.on('message', (messageObj) => {
+    const message = `${moment()
+      .format('DD-MM-yyyy HH:mm:ss')} - ${messageObj.nickname}: ${messageObj.chatMessage}`;
+    io.emit('message', message);
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
