bde23e0ebda65041c97cb75c90b8aaa48ef96e3c
MarceloBorges <melombm@hotmail.com>
2021-08-20 16:20:19 -0500

    requisito 2

--- socket/server.js
+++ socket/server.js
@@ -1,19 +1,28 @@
 // adriano me ajudou com essa função
 // github.com/adrianoforcellini
-
 const date = () => {
   const d = new Date();
   return `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}
   ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
 };
 
+const nickNames = [];
+// eslint-disable-next-line max-lines-per-function
 module.exports = (io) => {
   io.on('connection', async (socket) => {
+    socket.emit('nickname');
     socket.on('nickname', (data) => {
-      socket.broadcast.emit('name', data);
+      nickNames.push(data);
       socket.emit('name', data);
+      socket.emit('allNickNames', nickNames);
+      socket.broadcast.emit('allNickNames', nickNames);
     });
-
+    socket.on('changeName', (obj) => {
+      const { nickname, newName } = obj;
+      nickNames.splice(nickNames.indexOf(nickname), 1, newName);
+      socket.emit('allNickNames', nickNames);
+      socket.broadcast.emit('allNickNames', nickNames);
+    });
     socket.on('message', (data) => {
       const novaData = `${date()} ${data.nickname}:${data.chatMessage}`;
       socket.broadcast.emit('message', novaData);
