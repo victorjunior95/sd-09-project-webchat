7c6d456fd97192918934e5df234bcd1636acb525
antonioarieiro <sfgfffs@gmail.com>
2021-08-20 15:22:03 -0300

    fix use and route / for server

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 // Faça seu código aqui
-//init
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000; 
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'https://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+
+app.use(express.static(`${__dirname}/web`));
+
+require('./sockets/webChat')(io);
+
+app.get('/', (_req, res) => {
+    res.sendFile(`${__dirname}/web/index.html`);
+});
+
+http.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
