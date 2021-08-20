dcee4b6b32e1b8096ea982d45ea13d8077616838
Rafael <rafaeljavaro750@gmail.com>
2021-08-20 16:21:17 -0300

    Servidor de Pé

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => console.log(`Online on PORT: ${PORT}`));
