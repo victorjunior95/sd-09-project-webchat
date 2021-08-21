4c997e2764d6653b7d39865e8011d81b6683a8e0
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-08-21 04:05:09 -0300

    Configuraçao inicial do Socket.io

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(3000, () => console.log('Servidor rodando na porta 3000'));
