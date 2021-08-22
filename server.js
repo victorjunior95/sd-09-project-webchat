28442bbd9b5517a03d5b2e9fcc41fb96c49dd53a
patriciavsilva <silva.patricia.v@gmail.com>
2021-08-21 22:06:18 -0300

    Iniciando o projeto, AIII BRASIIIIL

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  } });
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
