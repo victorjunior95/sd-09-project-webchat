b638b718dba1cb4afb86956920e7bee0ae2f9900
Douglas Ferreira <dodofp.df@gmail.com>
2021-08-22 12:40:16 -0300

    Teste req4

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,9 +1,45 @@
 const Model = require('../models/messages');
 
+let clientList = [];
+
+const changeIndexPosition = (arr, fromIndex, toIndex) => {
+  const element = arr[fromIndex];
+  arr.splice(fromIndex, 1);
+  arr.splice(toIndex, 0, element);
+  return arr;
+};
+
+const disconnectUser = (socket) => {
+  socket.on('disconnect', () => {
+    const newClientFilteredList = clientList.filter((client) => client.id !== socket.id);
+    socket.broadcast.emit('desconectou', newClientFilteredList);
+    clientList = newClientFilteredList;
+  });
+};
+
+const changeUserNameInList = (socket) => {
+  socket.on('changeName', (name) => {
+    clientList.find((client) => client.id === socket.id).nickname = name;
+    socket.emit('list', clientList);
+    socket.broadcast.emit('list', changeIndexPosition(clientList, clientList.length - 1, 0));
+  });
+};
+
+const iJustGotHere = (socket) => {
+  socket.on('cheguei', (data) => {
+    clientList.push({ id: socket.id, nickname: data });
+    socket.broadcast.emit('list', clientList);
+    socket.emit('list', changeIndexPosition(clientList, clientList.length - 1, 0));
+  });
+};
+
 const chat = (io) => {
-  io.on('connection', (socket) => {    
+  io.on('connection', (socket) => {
+    iJustGotHere(socket);
+
+    changeUserNameInList(socket);
+
     socket.on('message', async (message) => {
-      console.log(message);
       const currentDate = (new Date()).toLocaleDateString().replaceAll('/', '-');
       const hour = (new Date()).toLocaleTimeString();
       const date = `${currentDate} ${hour}`;
@@ -14,6 +50,7 @@ const chat = (io) => {
 
       io.emit('message', `${date} ${message.nickname}: ${message.chatMessage}`);
     });
+    disconnectUser(socket);
   });
 };
 
