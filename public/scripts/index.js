7a1ddb5d6097dcabbc5e32d8ffb8413060f02701
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-08-22 10:50:29 -0300

    Requisito 2

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,7 +4,11 @@ let myId;
 const getSendMessageInput = () => document.querySelector('#message-to-send');
 
 const createNewMessage = (data) => {
-  const { chatMessage, messageUserId } = data;
+  //  WhatsApp Version
+  // const { chatMessage, messageUserId } = data;
+
+  //  Project Version
+  const { messageUserId } = data;
   const messagesBox = document.querySelector('#messages-box');
   // Message div
   const messageDiv = document.createElement('div');
@@ -16,7 +20,12 @@ const createNewMessage = (data) => {
 
   //  Message span
   const span = document.createElement('span');
-  span.innerText = chatMessage;
+  span.setAttribute('data-testid', 'message');
+  //  WhatsApp Version
+  // span.innerText = chatMessage;
+
+  //  Project Version
+  span.innerText = data;
   messageDiv.appendChild(span);
 
   messagesBox.appendChild(messageDiv);
@@ -30,7 +39,7 @@ const sendMessage = () => {
   const messageToSend = document.querySelector('#message-to-send').value;
   if (messageToSend === '') return;
   console.log(messageToSend);
-  socket.emit('message', { messageUserId: myId, chatMessage: messageToSend });
+  socket.emit('message', { nickname: myId, chatMessage: messageToSend });
   clearSendMessageInput();
 };
 
@@ -48,12 +57,45 @@ const sendEnterListener = () => {
   });
 };
 
+const showUserNickName = (name) => {
+  const div = document.querySelector('#online-user');
+  div.innerHTML = `${name}`;
+};
+
+const changeNickname = () => {
+  const nickname = document.querySelector('#nickname-box');
+  myId = nickname.value;
+  showUserNickName(myId);
+  nickname.value = '';
+};
+
+const changeNickNameListener = () => {
+  const saveBtn = document.querySelector('#nickname-button');
+  const input = document.querySelector('#nickname-box');
+
+  saveBtn.addEventListener('click', changeNickname);
+  input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      changeNickname();
+    }
+  });
+};
+
+const generateRandomName = () => {
+  //  https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  const rnd = (len, chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') => 
+  [...Array(len)].map(() => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
+  
+  myId = rnd(16);
+};
+
+//  Add listeners
 sendEnterListener();
 sendButtonListener();
+changeNickNameListener();
+
+generateRandomName();
+showUserNickName(myId);
 
 socket.emit('ping');
 socket.on('message', (data) => createNewMessage(data));
-socket.on('myId', (id) => {
-  myId = id;
-  console.log(myId);
-});
--- sockets/ping.js
+++ sockets/ping.js
@@ -5,13 +5,12 @@ const currentTime = moment().format('DD-MM-YYYY h:mm:ss');
 module.exports = (io) => io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
 
-  socket.emit('myId', socket.id);
-
   socket.on('ping', () => {
     console.log(`${socket.id} emitiu um ping!`);
   });
 
   socket.on('message', (data) => {
+    //  WhatsApp version
     // const { messageUserId, chatMessage } = data;
     // io.emit('message', { messageUserId, chatMessage });
 
