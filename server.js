838ddf3f98cf183b35bbc9ec2773fc351b6aa33f
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-08-22 14:22:55 -0300

    Requisito 3

--- server.js
+++ server.js
@@ -7,13 +7,36 @@ const io = require('socket.io')(http, {
     },
 });
 
+const connection = require('./models/connection');
+
+async function saveMessage(message, nickname) {
+    const conn = await connection();
+    const messages = conn.collection('messages');
+    messages.insertOne({ message, nickname, timestamp: new Date() });
+}
+
+function formatMessage(message, nickname, date) {
+    const dateFormatted = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    const timeFormatted = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    return `${dateFormatted} ${timeFormatted} ${nickname} ${message}`;
+}
+
+async function getMessages() {
+    const conn = await connection();
+    const messagesCollection = conn.collection('messages');
+    const messages = await messagesCollection.find().toArray();
+    return messages.map(({ message, nickname, timestamp }) =>
+     formatMessage(message, nickname, timestamp));
+}
+
 io.on('connection', (socket) => {
+    getMessages().then((messages) => {
+        socket.emit('messages', messages);
+    });
     socket.on('message', (data) => {
         const date = new Date();
-        const dateFormatted = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
-        const timeFormatted = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-        io.emit('message',
-         `${dateFormatted} ${timeFormatted} ${data.nickname} ${data.chatMessage}`);
+        saveMessage(data.chatMessage, data.nickname);
+        io.emit('message', formatMessage(data.chatMessage, data.nickname, date));
     });
 });
 app.get('/', (req, res) => {
