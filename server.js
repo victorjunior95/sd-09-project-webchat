ead88c04f4220998496a48ec1fc47ad847166c52
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-08-22 13:31:38 -0300

    Requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        const date = new Date();
+        const dateFormatted = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+        const timeFormatted = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        io.emit('message',
+         `${dateFormatted} ${timeFormatted} ${data.nickname} ${data.chatMessage}`);
+    });
+});
+
+http.listen(3000, () => console.log('server started'));
+
 // Faça seu código aqui
