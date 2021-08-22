521809f74baea07d1035e4bf244c8b82b1920bc4
Carine Becker <caarine.m@gmail.com>
2021-08-22 17:33:01 -0300

    feat req2 part of

--- server.js
+++ server.js
@@ -21,20 +21,33 @@ const messageDate = () => {
   const hour = new Date().toTimeString().split(' ')[0];
   return `${date} ${hour}`;
 };
+const messages = [];
 
 // o que faz quando um novo client se conecta, é conectado ao socket.io no front
 io.on('connection', (socket) => {
-  console.log(`Client ${socket.id} se conectou`);
+  const socketId = socket.id;
+  console.log(`Client ${socketId} se conectou`);
+
+  socket.emit('previousMessages', messages);
+
+  const randomId = socketId.slice(0, 16);
+  socket.emit('randomId', randomId);
 
   socket.on('message', ({ nickname, chatMessage }) => {
     const message = `${messageDate()} - ${nickname}: ${chatMessage}`;
-    io.emit('message', message);
+    messages.push(message);
+    io.emit('sentMessages', message);
     return message;
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.id} se desconectou`);
   });
 });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static('public'));
 
 app.use('/', (_req, res) => res.render('index.ejs'));
 
