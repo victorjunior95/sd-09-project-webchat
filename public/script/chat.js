138dee68bbe347186d402d3929fd818b49c87d1b
Alana Monteiro <lanaah.15@gmail.com>
2021-08-23 03:59:23 -0300

    requisito 2

--- public/script/chat.js
+++ public/script/chat.js
@@ -1,11 +1,22 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
+const sendButton = document.querySelector('#sendButton');
 const inputMessage = document.querySelector('#messageInput');
+const nicknameButton = document.querySelector('#nicknameButton');
+const inputNickname = document.querySelector('#nicknameBox');
 
-form.addEventListener('submit', (e) => {
+let nickname = '';
+
+nicknameButton.addEventListener('click', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id });
+  socket.emit('nicknameChange', inputNickname.value);
+  inputNickname.value = '';
+  return false;
+});
+
+sendButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  socket.emit('message', { chatMessage: inputMessage.value, nickname });
   inputMessage.value = '';
   return false;
 });
@@ -15,13 +26,36 @@ form.addEventListener('submit', (e) => {
 const newMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
-  console.log(message);
+  li.setAttribute('data-testid', 'message');
   li.innerText = message;
   messagesUl.appendChild(li);
 };
 
+// caso não encontre nenhum nickname, busca o socket.id e pega apenas 16 digitos dele
+// não foi possível definir o socket.id diretamente na declaraço da variavel pois a conexao nao havia sido estabelecida ainda
+
+const displayNickname = () => {
+  if (!nickname) nickname = socket.id.slice(1, 17);
+  const welcomeMessage = nickname;
+  const welcomeDiv = document.querySelector('#welcome');
+  const p = document.createElement('p');
+  p.setAttribute('data-testid', 'online-user');
+  p.setAttribute('id', 'onlineUser');
+  p.innerText = welcomeMessage;
+  welcomeDiv.appendChild(p);
+};
+
+const changeNickname = (newNickname) => {
+  console.log(nickname);
+  nickname = newNickname;
+  const userP = document.querySelector('#onlineUser');
+  const welcomeMessage = nickname;
+  userP.innerText = welcomeMessage;
+  return nickname;
+};
+
 // Quanto o evento welcome for emitido, a mensagem será tansformada num li pela funcao newMessage
-socket.on('welcome', (message) => newMessage(message));
+socket.on('welcome', () => displayNickname());
 socket.on('online', (message) => newMessage(message));
-socket.on('retorno', (message) => newMessage(message));
 socket.on('message', (chatMessage) => newMessage(chatMessage));
+socket.on('nicknameChange', (newNickname) => changeNickname(newNickname));
--- sockets/chat.js
+++ sockets/chat.js
@@ -4,8 +4,12 @@ const messageTime = moment().format('DD-MM-YYYY HH:mm:ss');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.emit('welcome', `welcome ${socket.id} to the TrybeChat`);
+    socket.emit('welcome', (nickname) => `welcome ${nickname} to the TrybeChat`);
 
+    socket.on('nicknameChange', (newNickname) => { 
+      io.emit('nicknameChange', newNickname);
+     });
+     
     socket.on('message', ({ chatMessage, nickname }) => {
       io.emit('message', `${messageTime} ${nickname} diz: ${chatMessage}`);
     });
