e55478e8fea77f01214518c006657b8a1da7b1c1
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-08-23 15:13:16 -0300

    FEAT - Requisito 02

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,10 +1,63 @@
 const socket = window.io();
 
-document.querySelector('form').addEventListener('submit', (event) => {
+const NICKNAMEBOX = document.querySelector('.nickname-box');
+const ONLINEUSER = document.querySelector('.online-user');
+const ONLINEUSERLI = document.querySelectorAll('.online-user li');
+const MESSAGEBOX = document.querySelector('.message-box');
+
+// adicionando nickname no input
+const inputNickname = (nickname) => {
+  NICKNAMEBOX.value = nickname;
+
+  if (ONLINEUSERLI.length) {
+    ONLINEUSER.innerHTML = '';
+  }
+
+  ONLINEUSER.insertAdjacentHTML('beforeend', `<li>${nickname}</li>`);
+};
+
+// gera nickname com 16 caracteres alfanumérico
+// https://www.codegrepper.com/code-examples/javascript/discord.js+random+character+generator
+const nicknameGenerator = () => {
+  let nickname = '';
+
+  const lengthString = 16;
+  const characteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let index = 0; index < lengthString; index += 1) {
+    nickname += characteres.charAt(
+      Math.floor(Math.random() * characteres.length),
+    );
+  }
+
+  inputNickname(nickname);
+};
+
+// exibindo a mensagem na tela
+const renderMessage = (message) => {
+  document
+    .querySelector('.messages')
+    .insertAdjacentHTML(
+      'beforeend',
+      `<div data-testid="message">${message}</div>`,
+    );
+};
+
+// adicionando nickname no campo input
+window.onload = () => {
+  nicknameGenerator();
+};
+
+// recebe mensagens do backend
+socket.on('message', (message) => {
+  renderMessage(message);
+});
+
+document.getElementById('form-chat').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const nickname = 'user';
-  const chatMessage = document.querySelector('.chatMessage').value;
+  const nickname = NICKNAMEBOX.value;
+  const chatMessage = MESSAGEBOX.value;
 
   if (nickname.length && chatMessage.length) {
     const dateActual = new Date().toLocaleString();
@@ -20,5 +73,15 @@ document.querySelector('form').addEventListener('submit', (event) => {
     socket.emit('message', sendMessage);
 
     chatMessage.value = '';
+  }
+});
+
+document.getElementById('form-nickname').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const nickname = document.querySelector('.nickname-box').value;
+
+  if (nickname.length) {
+    inputNickname(nickname);
   }
 });
