c4ec6247dc618eeeddda3ac5fdc14dfda34d6277
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-22 22:54:14 -0300

    Web Chat 2.2 - Separando a funçao auxiliar getNickName

--- public/js/EventesAndSockets.js
+++ public/js/EventesAndSockets.js
@@ -2,48 +2,25 @@ const socket = window.io();
 const nickDiv = '.nickNames';
 const { $ } = window;
 
-// ------------------------------------------------------------------------------------------//
-const adjectives = [
-  'lord', 'master', 'adorable', 'short', 'kind', 'tired', 'caring', 'fearless', 'funny',
-  'joyful', 'friendly', 'tall', 'short', 'handsome', 'determined', 'smart', 'studious', 'faithful',
-  'handy', 'honest', 'naive', 'insecure', 'fair', 'modest', 'neurotic', 'optimistic', 'daring',
-  'patient', 'romantic', 'nice', 'lucky', 'talented', 'shy'];
-
-const names = ['Carla', 'Tom', 'Jay', 'Martin', 'Sharon', 'Gustav', 'Wesley', 'Harry', 'Victor',
-  'Lorran', 'Brayan', 'Giulia', 'Karine', 'Ingrid', 'Yasmin', 'Emilly', 'Samara', 'Lilian',
-  'Ashley', 'Agatha', 'Hellen', 'Arna', 'Alyssa'];
-
-const getNickName = () => {
-  const random = (num) => Math.floor(Math.random() * num);
-  const randomAdj = adjectives[random(adjectives.length)];
-  const randonName = names[random(names.length)];
-  let nick = `${randomAdj}${randonName}`;
-  while (nick.length < 16) {
-    nick += random(9);
-  }
-  return nick;
-};
-// ------------------------------------------------------------------------------------------//
-
-let myNickName = getNickName();
+let myNickName = localStorage.getItem('MyNickName');
 
 const renderMessage = (message) => {
   $('.messages').append(
     `<div data-testid="message"  class="message">
-      ${message}
+    ${message}
+    </div>`,
+    );
+  };
+  
+  const renderNickNames = (username) => {
+    $(nickDiv).append(
+      `<div class="message">
+      <strong data-testid="online-user">${username}</strong>
       </div>`,
-  );
-};
-
-const renderNickNames = (username) => {
-  $(nickDiv).append(
-    `<div class="message">
-          <strong data-testid="online-user">${username}</strong>
-          </div>`,
-  );
-};
-renderNickNames(myNickName);
-
+      );
+    };
+    renderNickNames(myNickName);
+    
 socket.on('previousNames', (nicks) => {
   nicks.forEach((nick) => {
     renderNickNames(nick);
