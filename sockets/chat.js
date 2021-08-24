e25ba0a7d0432efe2fcc659c380c84c93a399977
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-08-24 20:17:26 -0300

    Comentado codigo referente ao erro do eslint

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,12 +1,13 @@
 const moment = require('moment'); // referência: https://tableless.com.br/trabalhando-com-moment/
 
-const users = [];
+// eslint-disable-next-line sonarjs/no-unused-collection
+// const users = [];
 
 module.exports = (io) => io.on('connection', (socket) => {
   socket.emit('serverMessage', 'Bem vindo ao chat!');
   
   socket.on('login', async (nickName) => {
-    users.push(nickName);
+    // users.push(nickName);
   });
 
   socket.on('message', ({ chatMessage, nickname }) => {
