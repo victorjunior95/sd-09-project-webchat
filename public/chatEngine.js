b14133db01ea949ed1c30e7a7568079764f1aae6
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-08-24 11:41:37 -0300

    Requisitos 1 e 2

---
+++ public/chatEngine.js
@@ -0,0 +1,51 @@
+const socket = window.io();
+
+const messageBtn = document.querySelector('#messageBtn');
+const user = document.querySelector('#user');
+const messageInput = document.querySelector('#messageInput');
+const nickNameInput = document.querySelector('#nickName');
+const messages = document.querySelector('#messages');
+const onlineUser = document.querySelector('#onlineUser');
+
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+const getNickName = (length) => {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.';
+  let result = '';
+  for (let i = 0; i < length; i += 1) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+};
+
+const nickName = getNickName(16);
+localStorage.setItem('nickname', nickName);
+
+messageBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const storeNickname = await localStorage.getItem('nickname');
+  socket.emit('message', { chatMessage: messageInput.value, nickname: storeNickname });
+  messageInput.value = '';
+  return false;
+});
+
+user.addEventListener('click', (e) => {
+  e.preventDefault();
+  const userNickname = nickNameInput.value;
+  onlineUser.innerText = userNickname;
+  localStorage.setItem('nickname', userNickname);
+  socket.emit('nickname', userNickname);
+  nickNameInput.value = '';
+  return false;
+});
+
+socket.on('newConnection', () => {
+  const storeNickname = localStorage.getItem('nickname');
+  onlineUser.innerText = storeNickname;
+});
+
+socket.on('message', (message) => {
+  const incomingMessage = document.createElement('li');
+  incomingMessage.setAttribute('data-testid', 'message');
+  incomingMessage.innerText = message;
+  messages.appendChild(incomingMessage);
+});
--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const socketServer = http.createServer(app);
+
+const io = require('socket.io')(socketServer, {
+  cors: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+});
+
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.use(cors());
+
+require('./sockets')(io);
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/view.html')));
+
+socketServer.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
