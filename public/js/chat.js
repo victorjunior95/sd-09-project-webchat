2e4b0f4df6356f9c7f38f7b95c74dcb072cb826c
Rafael <rafaeljavaro750@gmail.com>
2021-08-25 13:59:47 -0300

    Foi

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,11 +4,12 @@ const sendButton = document.querySelector('#sendButton');
 const inputMessage = document.querySelector('#messageInput');
 const newNickNameInput = document.querySelector('#nicknameInput');
 const setNewNickButton = document.querySelector('#nickButton');
+const listUl = document.querySelector('#listOnlineUsers');
 let nickname = '';
 
-setNewNickButton.addEventListener('click', (e) => {
-  e.preventDefault();
+setNewNickButton.addEventListener('click', () => {
   nickname = newNickNameInput.value;
+  socket.emit('newNickName', nickname);
   newNickNameInput.value = '';
 });
 
@@ -28,13 +29,19 @@ const createMessage = (message) => {
 
 socket.on('message', (message) => createMessage(message));
 socket.on('listUsers', (users) => {
-  const listUl = document.querySelector('#listOnlineUsers');
-  const li = document.createElement('li');
+  listUl.innerHTML = '';
+  const myLi = document.createElement('li');
+  myLi.setAttribute('data-testid', 'online-user');
+  myLi.innerText = nickname || socket.id.slice(0, 16);
+  listUl.appendChild(myLi);
   users.forEach((user) => {
-    li.innerText = user;
+    if (user.nickname !== nickname) {
+     const li = document.createElement('li');
+     li.setAttribute('data-testid', 'online-user');
+     li.innerText = user.nickname;
+     listUl.appendChild(li);
+   }
   });
-  li.setAttribute('data-testid', 'online-user');
-  listUl.appendChild(li);
 });
 
 window.onbeforeunload = () => {
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,15 +1,24 @@
 const moment = require('moment');
 const { saveMessage } = require('../models/chat');
 
-module.exports = (io) => io.on('connection', async (socket) => {
-  const users = [];
-  const firstNickname = socket.id.slice(0, 16);
-  users.push(firstNickname);
-  io.emit('listUsers', users);
+let users = [];
 
+module.exports = (io) => io.on('connection', async (socket) => {
+  const firstNickname = socket.id.slice(0, 16);
+  users.push({ nickname: firstNickname, socketId: socket.id });
+  io.emit('listUsers', users);
   const timeStamp = moment().format('DD-MM-yyyy LTS');
   socket.on('message', async ({ chatMessage, nickname }) => {
     await saveMessage({ message: chatMessage, nickname, timeStamp });
     io.emit('message', `${timeStamp} - ${nickname}: ${chatMessage}`);
+  });
+  socket.on('newNickName', (nickname) => {
+    users = users.map((user) => {
+      if (user.socketId === socket.id) return { ...user, nickname };
+        return user;
+    }); io.emit('listUsers', users);
+  });
+  socket.on('disconnect', () => { 
+    users = users.filter((user) => user.socketId !== socket.id); io.emit('listUsers', users);
   });
 });
