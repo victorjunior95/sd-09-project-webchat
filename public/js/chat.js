f075b9ee7ab4412d3cbbe84906f6615c7052f6cf
Renato Feitosa <pythaqua@gmail.com>
2021-08-25 03:38:23 -0300

    chore: Add 4th requirement (without refact)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,45 +1,71 @@
 const socket = window.io();
 
+const MESSAGE_EVENT = 'message';
+const SET_NICKNAME_EVENT = 'setNickname';
+const ONLINE_USERS_EVENT = 'onlineUsers';
+const DISCONNECT_EVENT = 'disconnect';
+
 const messageForm = document.querySelector('#messageForm');
 const inputMessage = document.querySelector('#messageInput');
 const nicknameForm = document.querySelector('#nicknameForm');
 const inputNickname = document.querySelector('#nicknameInput');
-const nicknameWrapper = document.querySelector('#userNickname');
+const onlineUsersList = document.querySelector('#onlineUsers');
 
-let nickname;
+let clientNickname;
+let clientNicknameLi;
 
-const setClientNickname = (givenNickname) => {
-  nickname = givenNickname;
-  nicknameWrapper.innerText = nickname;
+const createUserElement = (nickname, userClass) => {
+  const li = document.createElement('li');
+  li.innerText = nickname;
+  li.classList.toggle(userClass);
+  li.dataset.testid = 'online-user';
+  onlineUsersList.appendChild(li);
+  return li;
+};
+
+const renderClientNickname = (givenNickname) => {
+  const hasNicknameChanged = clientNickname && givenNickname !== clientNickname;
+  const element = hasNicknameChanged
+    ? clientNicknameLi
+    : createUserElement(givenNickname, 'me');
+  clientNickname = givenNickname;
+  clientNicknameLi = element;
+  element.innerText = clientNickname;
 };
 
 const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
-  li['data-testid'] = 'message';
-  // li[data-testid] = 'message';
   li.dataset.testid = 'message';
-  console.log(li);
   messagesUl.appendChild(li);
 };
 
-socket.on('setNickname', (givenNickname) => setClientNickname(givenNickname));
-socket.on('message', (message) => createMessage(message));
+const renderOnlineUsers = (users) => {
+  let otherUsers = users.filter((user) => user !== clientNickname);
+  const rendered = document.querySelectorAll('.others');
+  rendered.forEach((el) => el.remove());
+  // const otherUsers = users.filter((user) => user !== clientNickname);
+  otherUsers.forEach((user) => createUserElement(user, 'others'));
+};
 
-messageForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+socket.on(SET_NICKNAME_EVENT, (givenNickname) => renderClientNickname(givenNickname));
+socket.on(MESSAGE_EVENT, (message) => createMessage(message));
+socket.on(ONLINE_USERS_EVENT, (onlineUsers) => renderOnlineUsers(onlineUsers));
+
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
   const chatMessage = inputMessage.value;
-  socket.emit('message', { nickname, chatMessage });
+  socket.emit(MESSAGE_EVENT, { nickname: clientNickname, chatMessage });
   inputMessage.value = '';
   return false;
 });
 
-nicknameForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+nicknameForm.addEventListener('submit', (event) => {
+  event.preventDefault();
   const newNickname = inputNickname.value;
-  socket.emit('setNickname', newNickname);
-  setClientNickname(newNickname);
+  socket.emit(SET_NICKNAME_EVENT, newNickname);
+  renderClientNickname(newNickname);
   inputNickname.value = '';
   return false;
 });
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,20 +1,46 @@
 const getDatetimeStr = require('../utils/getDatetimeStr');
 
-module.exports = (io) => io.on('connection', (socket) => {
+const MESSAGE_EVENT = 'message';
+const SET_NICKNAME_EVENT = 'setNickname';
+const ONLINE_USERS_EVENT = 'onlineUsers';
+const DISCONNECT_EVENT = 'disconnect';
+
+let onlineUsers = [];
+
+const sendToAllClients = (io, event, content) => io.emit(event, content);
+
+const sendToThisClient = (socket, event, content) => socket.emit(event, content);
+
+const sendToOtherClients = (socket, event, content) => socket.broadcast.emit(event, content);
+
+const startChatClient = (io) => (socket) => {
   let clientNickname = socket.id.toString().slice(0, 16);
-  socket.emit('setNickname', clientNickname);
-  socket.on('setNickname', (newNickname) => {
-    io.emit('message', `${clientNickname} mudou seu nick para ${newNickname}`);
+  onlineUsers.push(clientNickname);
+  console.log('user', clientNickname);
+  console.log('users', onlineUsers);
+
+  sendToThisClient(socket, SET_NICKNAME_EVENT, clientNickname);
+  sendToAllClients(io, ONLINE_USERS_EVENT, onlineUsers);
+  
+  socket.on(SET_NICKNAME_EVENT, (newNickname) => {
+    io.emit(MESSAGE_EVENT, `${clientNickname} mudou seu nick para ${newNickname}`);
+    onlineUsers = onlineUsers
+    .map((nickname) => ((nickname === clientNickname) ? newNickname : nickname));
     clientNickname = newNickname;
+    sendToAllClients(io, ONLINE_USERS_EVENT, onlineUsers);
   });
 
-  socket.on('message', ({ nickname, chatMessage }) => {
+  socket.on(MESSAGE_EVENT, ({ nickname, chatMessage }) => {
     const datetimeStr = getDatetimeStr();
     const messageStr = `${datetimeStr} - ${nickname}: ${chatMessage}`;
-    io.emit('message', messageStr);
+    sendToAllClients(io, MESSAGE_EVENT, messageStr);
   });
   
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('serverMessage', `Xiii! ${socket.id} acabou de se desconectar! :(`);
+  socket.on(DISCONNECT_EVENT, () => {
+    socket.broadcast.emit(MESSAGE_EVENT, `${clientNickname} deixou a sala`);
+    onlineUsers = onlineUsers.filter((user) => user !== clientNickname);
+    sendToAllClients(io, ONLINE_USERS_EVENT, onlineUsers);
   });
-});
+};
+
+module.exports = (io) => io.on('connection', startChatClient(io));
