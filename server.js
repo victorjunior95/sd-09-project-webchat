8b7eff1caa2e9a3185b8b950435a10bb27cd0424
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-08-25 00:53:34 -0300

    webchat

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-// Faça seu código aqui
+const express = require('express');
+const moment = require('moment');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(`${__dirname}/public`));
+
+const chat = require('./models/chat');
+
+let users = [];
+const getUsers = () => users.map(({ nickname }) => nickname);
+
+io.on('connect', async (socket) => {
+  // nickName aleatorio
+  const randonNickname = socket.id.slice(0, 16);
+  users.push({ socket, nickname: randonNickname });
+  socket.emit('changeNickname', randonNickname); io.emit('allUsers', getUsers());
+  socket.emit('chatMongoDB', await chat.getMessages());
+
+  // Enviar msg e add timestamp
+  socket.on('message', async ({ nickname, chatMessage }) => {
+    const timestamp = moment().format('DD-MM-yyyy HH:mm:ss A');
+    const message = `${timestamp} - ${nickname}: ${chatMessage} \n`;
+    await chat.createMessage({ message, timestamp: moment().toISOString(), nickname });
+    io.emit('message', message);
+  });
+
+  // trocar nickName
+  socket.on('changeNickname', (payload) => {
+    const findUser = users.find((user) => user.socket === socket); 
+    findUser.nickname = payload; io.emit('allUsers', getUsers());
+  });
+
+  // remover da lista de usuarios ativos
+  socket.on('disconnect', () => {
+    users = users.filter((user) => user.socket !== socket);
+    io.emit('allUsers', getUsers());
+  });
+});
+
+app.use('/', (_req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+server.listen(3000, () => console.log('Listening on port 3000'));
